e2b0535acc492056b59efe33bdd6561c7c90f7fa
puertorica3200-afk <puertorica3200@gmail.com>
2026-09-04 09:09:49 +0000

    setted up the neon database and added a test def

--- Oscuola/app/main.py
+++ Oscuola/app/main.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-
-
-
+import database
+from Oscuola.app.database.fetch import test
 
 app = FastAPI()
 @app.get("/")
 def root():
-    return {"message": "online "}
+    s=test()
+    return {"message": s}
