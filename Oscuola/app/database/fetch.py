df6c827d901a34371db31a376df8bde099efd62c
puertorica3200-afk <puertorica3200@gmail.com>
2026-09-04 11:14:07 +0000

    built the check pipeline , app->vercel-endpoint->neondb, starting tests

--- Oscuola/app/database/fetch.py
+++ Oscuola/app/database/fetch.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import psycopg2
 import os
+import bcrypt
 def test():
     load_dotenv()
     cons=os.environ["CON_STRING"]
@@ -12,6 +13,21 @@ def test():
     s.close()
     return res
 
-
+def check_login(gmail,pswd):
+    load_dotenv()
+    cons=os.environ["CON_STRING"]
+    s=psycopg2.connect(os.environ["DATABASE_URL"])
+    cur = s.cursor()
+    pswd.encode()
+    bcrypt.gensalt()
+    hs= bcrypt.hashpw(pswd.encode(), bcrypt.gensalt())
+    cur.execute("SELECT * FROM users WHERE gmail=%s AND password=%s ;")(gmail,hs)
+    res=(cur.fetchone())
+    cur.close()
+    s.close()
+    if res:
+       return "pass"
+    else:
+        return "no"
 
 
--- Oscuola/app/main.py
+++ Oscuola/app/main.py
@@ -2,10 +2,23 @@ import sys
 import os
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from fastapi import FastAPI
-from database.fetch import test
+from database.fetch import test,check_login
 app = FastAPI()
-
+from fastapi import Header, HTTPException
+def verify_key(authorization: str = Header(None)):
+    if authorization != f"Bearer {os.environ['auth_key']}":
+        raise HTTPException(status_code=401, detail="Unauthorized ass bitch")
 @app.get("/")
 def root():
     s = test()
     return {"message": s}
+
+@app.post("/login_check")
+def check(data):
+    verify_key()
+    if(check_login(data.gmail,data.password)=="pass"):
+        return {"message": "pass"}
+    else:
+        return {"message": "not found"}
+
+
