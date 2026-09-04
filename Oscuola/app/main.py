34f2142b386d2c87fced0622f21ad088a98bc9b8
Eve-boo <zeinbamdouni@gmail.com>
2026-09-04 09:17:50 +0000

    importlib fix

--- Oscuola/app/main.py
+++ Oscuola/app/main.py
@@ -1,8 +1,11 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from fastapi import FastAPI
 from database.fetch import test
-
 app = FastAPI()
+
 @app.get("/")
 def root():
-    s=test()
+    s = test()
     return {"message": s}
