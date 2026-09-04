53a6f1580b4a5ee5a49db01a903ea7270b194bc8
mohamed amdouni <midouamdouni4@gmail.com>
2026-09-04 11:14:07 +0000

    built the check pipeline , app->vercel-endpoint->neondb, starting tests

--- Oscuola/mainwindow.cpp
+++ Oscuola/mainwindow.cpp
@@ -1,21 +1,25 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
-MainWindow::MainWindow(QWidget *parent)
+#include "database.h"
+MainWindow::MainWindow(database& dbo,QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow){
+    , ui(new Ui::MainWindow),db(dbo){
     ui->setupUi(this);
     setFixedSize(1280, 720);
     connect(ui->login_but,&QPushButton::clicked,this,[this]() {
         if(ui->login_email->text()==""){
             ui->login_alert->setText("fill all fields please");
+        }else{
+            if(db.login_check(ui->login_email->text().toStdString(),ui->login_passwd->text().toStdString())){
+                switchpg(2);
+            }else{
+                ui->login_alert->setText("user not found");
+            }
         }
-
     });
 
     connect(ui->reg_but,&QPushButton::clicked,this,[this]() {
         switchpg(1);
-
     });
 
 
