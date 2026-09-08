5bca35247d29a40568ccf6b6d2b26c45ddae80e0
puertorica3200-afk <puertorica3200@gmail.com>
2026-09-08 20:28:05 +0000

    navigation is established , next is grouping the buttons across all the student pages

--- Oscuola/mainwindow.cpp
+++ Oscuola/mainwindow.cpp
@@ -5,14 +5,32 @@ MainWindow::MainWindow(database& dbo,QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow),db(dbo){
     ui->setupUi(this);
+    switchpg(2);
     setFixedSize(1280, 720);
+    connect(ui->grades_button_student,&QPushButton::clicked,this,[this]() {
+        switchpg(3);
+    });
+    connect(ui->alerts_student_but,&QPushButton::clicked,this,[this]() {
+        switchpg(4);
+    });
+    connect(ui->post_button_student,&QPushButton::clicked,this,[this]() {
+        switchpg(5);
+    });
+    connect(ui->timetable_but_student,&QPushButton::clicked,this,[this]() {
+        switchpg(6);
+    });
+
+
+
     connect(ui->login_but,&QPushButton::clicked,this,[this]() {
         if(ui->login_email->text()==""){
             ui->login_alert->setText("fill all fields please");
         }else{
             std::tuple<std::string, std::string, std::string, std::string> s=db.login_check(ui->login_email->text().toStdString(), ui->login_passwd->text().toStdString());
             if (get<0>(s)!="false") {
-                switchpg(2);
+                if(get<0>(s)=="student"){
+                    switchpg(2);
+                }
                 QByteArray pfp = QByteArray::fromBase64(QString::fromStdString(std::get<3>(s)).toUtf8());
                 QPixmap p;
                 if (!p.loadFromData(pfp, "JPEG")) {
@@ -31,7 +49,7 @@ MainWindow::MainWindow(database& dbo,QWidget *parent)
                 ui->login_alert->setText("user not found");
             }
         }});
-    connect(ui->submit,&QPushButton::clicked,this,[this]() {
+    connect(ui->reg_but,&QPushButton::clicked,this,[this]() {
         if(ui->reg_email->text()=="" || ui->reg_pswd->text()=="" || ui->pswd_conf->text()==""){
             ui->login_alert->setText("fill all fields please(reg)");
         }else if(ui->reg_pswd->text()!= ui->pswd_conf->text()){
