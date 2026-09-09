81a3852258a5f329f15e1da5f2e41d7e4f13b23a
mohamed amdouni <154617033+mohamed-adhd@users.noreply.github.com>
2026-09-09 12:53:34 +0000

    grades fetched by login-returned id

--- Oscuola/database.h
+++ Oscuola/database.h
@@ -9,8 +9,8 @@ class database : public QObject{
     Q_OBJECT
 private:
 public :
-     QMap<QString, double> st1_student_grade(int);
-    std::tuple<std::string, std::string, std::string, std::string> login_check(std::string email, std::string passwd);
+    QMap<QString, double> st1_student_grade(int);
+    std::tuple<std::string, std::string, std::string, std::string,int> login_check(std::string email, std::string passwd);
     void registerr(std::string email, std::string passwd, std::function<void(bool)> callback);
     database(QObject *parent = nullptr) : QObject(parent)
     {
--- Oscuola/mainwindow.cpp
+++ Oscuola/mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(database& dbo,QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow),db(dbo){
     ui->setupUi(this);
-    switchpg(2);
+    switchpg(0);
     setFixedSize(1280, 720);
     QMap<QString, double> s=db.st1_student_grade(1);
     for (int row = 0; row < ui->grades_table->rowCount(); ++row) {
@@ -20,6 +20,26 @@ MainWindow::MainWindow(database& dbo,QWidget *parent)
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     QButtonGroup *grades_buts= new QButtonGroup(this);
     for(int i=1;i<5;i++){
         QString name=QString("grades_button_student_%1").arg(i);
@@ -27,7 +47,18 @@ MainWindow::MainWindow(database& dbo,QWidget *parent)
         if(button){
             grades_buts->addButton(button);
         }}
-    connect(grades_buts,&QButtonGroup::buttonClicked,this,[this]() {
+    connect(grades_buts,&QButtonGroup::buttonClicked,this,[this](QAbstractButton*) {
+        if (!loaded_grades){
+            QMap<QString, double> ss=db.st1_student_grade(std::get<4>(f));
+            for (int row = 0; row < ui->grades_table->rowCount(); ++row) {
+                QString subject = ui->grades_table->item(row, 0)->text();
+                if (ss.contains(subject)) {
+                    double value = ss[subject];
+                    ui->grades_table->setItem(row, 1, new QTableWidgetItem(QString::number(value)));
+                }
+            }
+            loaded_grades=true;
+        }
 
         switchpg(3);});
 
@@ -85,7 +116,8 @@ MainWindow::MainWindow(database& dbo,QWidget *parent)
         if(ui->login_email->text()==""){
             ui->login_alert->setText("fill all fields please");
         }else{
-            std::tuple<std::string, std::string, std::string, std::string> s=db.login_check(ui->login_email->text().toStdString(), ui->login_passwd->text().toStdString());
+            std::tuple<std::string, std::string, std::string, std::string,int> s=db.login_check(ui->login_email->text().toStdString(), ui->login_passwd->text().toStdString());
+            f=s;
             if (get<0>(s)!="false") {
                 if(get<0>(s)=="student"){
                     switchpg(2);
@@ -116,17 +148,17 @@ MainWindow::MainWindow(database& dbo,QWidget *parent)
         }
         else{
             db.registerr(ui->reg_email->text().toStdString(),ui->reg_pswd->text().toStdString(),[this](bool success){
-                       if (success) {
-                            ui->reg_alert->setText("we have submitted you account request , you will be notified by email when done ");
-                       } else {
-                           ui->reg_alert->setText("something went wrong , try again later");
-                       }
-                                                                   });}});
+                if (success) {
+                    ui->reg_alert->setText("we have submitted you account request , you will be notified by email when done ");
+                } else {
+                    ui->reg_alert->setText("something went wrong , try again later");
+                }
+            });}});
 
 
     connect(ui->reg_but,&QPushButton::clicked,this,[this]() {
         switchpg(1);
-});
+    });
 
 
 
