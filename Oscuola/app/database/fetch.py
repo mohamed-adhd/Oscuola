13d7d04fef87ce0666f360ab0d6da60fe9d25c9e
mohamed-adhd <midouamdouni4@gmail.com>
2026-09-12 19:20:12 +0000

    endpoint add

--- Oscuola/app/database/fetch.py
+++ Oscuola/app/database/fetch.py
@@ -114,6 +114,21 @@ def get_grades_1st(id):
 
 
 
+def get_classes(id):
+    load_dotenv()
+    cons = os.environ["CON_STRING"]
+    s = psycopg2.connect(os.environ["DATABASE_URL"])
+    cur = s.cursor()
+    cur.execute("SELECT yeary,classs FROM classes WHERE teacher_id = %s ;", (id,))
+    res = cur.fetchall()
+    cur.close()
+    s.close()
+    ss = {
+        "success": True,
+        "data": res
+    }
+    return ss
+
 
 
 
