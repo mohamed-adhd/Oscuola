944a47dce376b7adecabe3107abcb543b7ba0de0
mohamed-adhd <midouamdouni4@gmail.com>
2026-09-20 17:21:27 +0000

    added 2nd/3rd year grades fetcher

--- Oscuola/app/database/fetch.py
+++ Oscuola/app/database/fetch.py
@@ -110,6 +110,33 @@ def get_grades_1st(id):
     s.close()
     return {"success": True, "math": res[2], "french": res[3], "english": res[4],"cs":res[5],"ph": res[6], "scvt": res[7], "overallg": res[8]}
 
+
+
+def get_grades_2nd(id):
+    load_dotenv()
+    cons = os.environ["CON_STRING"]
+    s = psycopg2.connect(os.environ["DATABASE_URL"])
+    cur = s.cursor()
+    cur.execute("SELECT * FROM second_year_grades WHERE student_id = %s ;", (id,))
+    res = cur.fetchone()
+    cur.close()
+    s.close()
+    return {"success": True, "math": res[2], "french": res[3], "english": res[4],"cs":res[5],"ph": res[6], "scvt": res[7], "overallg": res[8]}
+
+
+
+
+def get_grades_3rd(id):
+    load_dotenv()
+    cons = os.environ["CON_STRING"]
+    s = psycopg2.connect(os.environ["DATABASE_URL"])
+    cur = s.cursor()
+    cur.execute("SELECT * FROM third_year_grades WHERE student_id = %s ;", (id,))
+    res = cur.fetchone()
+    cur.close()
+    s.close()
+    return {"success": True, "math": res[2], "french": res[3], "english": res[4],"cs":res[5],"ph": res[6], "scvt": res[7], "overallg": res[8]}
+
 def getstudents(classs,year):
     load_dotenv()
     cons = os.environ["CON_STRING"]
