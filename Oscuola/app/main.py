349436936aed732ac515851d709dbc689c1ce1fe
mohamed-adhd <midouamdouni4@gmail.com>
2026-09-25 20:00:28 +0000

    endpoint syntax fix

--- Oscuola/app/main.py
+++ Oscuola/app/main.py
@@ -100,7 +100,7 @@ def syst(data : ids, authorized: None = Depends(verify_key)):
         return get_grades_1st(data.id)
     elif get_get_year(data.id)=="2" :
         return get_grades_2nd(data.id)
-    elif get_get_year(data.id)=="3" :
+    elif get_year(data.id)=="3" :
         return get_grades_3rd(data.id)
 
 
